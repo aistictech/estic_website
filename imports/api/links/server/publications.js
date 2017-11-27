d2387be15593b717364668f6bc4aa8261c04c8a9
MYCS <samsonmolou@gmail.com>
2017-11-27 08:58:30 +0000

    Update to meteor 1.6.0

--- imports/api/links/server/publications.js
+++ imports/api/links/server/publications.js
@@ -1,14 +1,8 @@
 // All links-related publications
 
 import { Meteor } from 'meteor/meteor';
-<<<<<<< HEAD
 import Links from '../links.js';
 
 Meteor.publish('links.all', function linksAll() {
-=======
-import { Links } from '../links.js';
-
-Meteor.publish('links.all', function () {
->>>>>>> 4f829a725b729b389875cf27e5202252c3c61920
   return Links.find();
 });
--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,7 +1,3 @@
 // Import client startup through a single index entry point
 
-<<<<<<< HEAD
 import './routes/';
-=======
-import './routes.js';
->>>>>>> 4f829a725b729b389875cf27e5202252c3c61920
