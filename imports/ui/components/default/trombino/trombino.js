87aeb3b300415715dfecbb2eb2a14e7bf65c4136
MYCS <samsonmolou@gmail.com>
2017-12-03 11:16:09 +0000

    Update trombino

--- imports/ui/components/default/trombino/trombino.js
+++ imports/ui/components/default/trombino/trombino.js
@@ -1,1 +1,14 @@
 import './trombino.html';
+
+Template.trombino.onRendered(function() {
+  $('.slider').slider({
+    indicators: false,
+    height: '700px',
+  });
+});
+
+Template.trombino.helpers({
+});
+
+Template.trombino.events({
+});
