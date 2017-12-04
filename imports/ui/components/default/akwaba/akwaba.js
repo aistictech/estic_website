60f1e28e2d2b356400f03979c9001c9b3d4a8714
camille225 <makaye151@gmail.com>
2017-12-04 22:27:50 +0000

    Akwaba update

--- imports/ui/components/default/akwaba/akwaba.js
+++ imports/ui/components/default/akwaba/akwaba.js
@@ -1,1 +1,16 @@
 import './akwaba.html';
+
+Template.akwaba.onRendered(function() {
+    $('.slider').slider({
+      indicators: false,
+      height: '600px',
+    });
+  });
+  
+  Template.akwaba.helpers({
+      
+  });
+  
+  Template.akwaba.events({
+
+  });
